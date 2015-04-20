fadeccc4c2b271dcddfa8261080fdec227670d2c
Yedidya Weiner <ydweiner17@gmail.com>
2015-04-19 22:56:07 -0500

    completed birthday until today functionality

--- sundays.rb
+++ sundays.rb
@@ -1,14 +1,27 @@
 require 'date'
 
-x = Date.parse("1992", "9", "17") 
+# checking Sundays that fell on the first of the month since user's birthday
 
+puts "Please enter you birthdate."
+puts "Year:"
+birth_year = gets.chomp.to_i
+puts "Month (as a number):"
+birth_month = gets.chomp.to_i
+puts "Day (as a number):"
+birth_day = gets.chomp.to_i
+
+user_bday = Date.new(birth_year, birth_month, birth_day) 
+
+# counts sundays that land on 1st of month from users birthdate until today
 sundays = 0
 
-x.upto(Date.today) do |date|
+user_bday.upto(Date.today) do |date|
   if date.sunday? && date.mday == 01
     sundays += 1
   end
 end
+
+puts "#{sundays} Sundays fell on the 1st of the month from your birthday until today. "
 
 
 
